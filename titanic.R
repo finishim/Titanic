199ff538da601cc61451bbda03ea96728d8477cb
Nazmi Anik <nazmianik@gmail.com>
2018-02-14 10:46:17 -0500

    Investigate the Dataframes

--- titanic.R
+++ titanic.R
@@ -1,3 +1,11 @@
 # Import the training and testing sets
 train_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
 test_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+# Create the dataframes
+train <- read.csv(train_url)
+test <- read.csv(test_url)
+# Investigate the data
+## Two-way comparison: Sex and Survived
+table(train$Sex, train$Survived)
+## Two-way comparison: row-wise proportions
+prop.table(table(train$Sex, train$Survived),1)
