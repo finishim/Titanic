dbe81081b516dae40dcf1606c4aa9650f86cd0bb
Nazmi Anik <nazmianik@gmail.com>
2018-02-14 10:37:14 -0500

    Import Datasets

---
+++ titanic.R
@@ -0,0 +1,3 @@
+# Import the training and testing sets
+train_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+test_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
